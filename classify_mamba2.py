e8e04869a1c4eafff3cd679fb593eba8240dbbc1
redtea-code <59169512+redtea-code@users.noreply.github.com>
2025-02-25 12:42:20 +0800

    Update classify_mamba2.py

--- classify_mamba2.py
+++ classify_mamba2.py
@@ -643,6 +643,83 @@ def main4(args):
             torch.save(ft_model.state_dict(), j(dir, f'model_current/ft_model_current.pth'))
             print('Saved model')
     file.close()
+
+def save_result(args,dataset=False):
+    cf = load_config(args.config_path)
+    device = 'cuda'
+    use_best = True
+
+
+    val_dataloader = classi_dataloader(cf['eval_path'],
+                                       cf['img_sz'],
+                                       cf['eval_bc'],
+                                       cf['table_path'],
+                                       True,
+                                       days_threshold=cf['days_threshold'],use_OASIS=dataset)
+    ref_model = Residual_mid_UNet3D_vit(1, 1, is_segmentation=False, f_maps=(64, 128, 256))
+    ref_model.load_state_dict(torch.load('weights/model.pt'))
+
+    if dataset:
+        table_df = val_dataloader.dataset.clinical_data
+    else:
+        table_df = val_dataloader.dataset.table_df
+    X = torch.cat([torch.tensor(table_df['cate_x'].values, dtype=torch.float32),
+                   torch.tensor(table_df['conti_x'].values, dtype=torch.float32)], dim=1)
+    graphs_dataset_all = build_graph_from_table(X)
+    ft_model = Graph_mamba_(
+        categories=table_df['num_cat'],  # tuple containing the number of unique values within each category
+        num_continuous=table_df['num_cont'],  # number of continuous values
+        dim=cf['dim'],  # dimension, paper set at 32
+        dim_out=cf['dim_out'],  # binary prediction, but could be anything
+        depth=cf['depth'],  # depth, paper recommended 6
+        heads=cf['heads'],  # heads, paper recommends 8
+        X=X,
+        graphs_dataset_all=graphs_dataset_all
+    )
+
+    ref_model.eval()
+    ref_model = ref_model.to(device)
+    # model = Combine_classfier_vit_mid()
+    if use_best:
+        ft_model.load_state_dict(torch.load(j(cf['pth_dir'],'model_best', 'best_ft_model.pth')))
+        # model.load_state_dict(torch.load(j(cf['pth_dir'], 'best_model.pth')))
+    else:
+        ft_model.load_state_dict(torch.load(j(cf['pth_dir'],'model_current', 'ft_model_current.pth')))
+        # model.load_state_dict(torch.load(j(cf['pth_dir'], 'model_current.pth')))
+
+    loss_fn = nn.BCELoss()
+    ft_model = ft_model.to(device)
+    
+    log_dir = j(cf['pth_dir'], 'result.txt')
+    file = open(log_dir, "w")
+    
+    for epoch in range(1):
+    
+        val_ft_model = ft_model.eval()
+        all_predictions = []
+        all_targets = []
+        correct = 0
+        losses = 0
+        total = 0
+        for i, batch in tqdm(enumerate(val_dataloader), total=len(val_dataloader)):
+            x, x_cat, x_num, y = batch['image'], batch['cate_x'], batch['conti_x'], batch['label'].float()
+            x, x_cat, x_num, y = x.to(device), x_cat.to(device), x_num.to(device), y.to(device)
+            with torch.no_grad():
+                mid_input, mid_output, PET = ref_model(x, output_vit_mid=True)
+                pred = val_ft_model(x_cat, x_num, [x, PET])
+            pred = F.sigmoid(pred)
+            loss = loss_fn(pred.squeeze(1), y)
+            pred_labels = pred.round()
+            total += y.size(0)
+            correct += (pred_labels.squeeze(1) == y).sum().item()
+            losses += loss.item()
+            all_predictions.append(y)
+            all_targets.append(pred_labels.squeeze(1))
+        lines = [f"{int(l1)},{int(l2)}\n" for l1, l2 in zip(all_predictions, all_targets)]
+        file.writelines(lines)
+        file.flush()
+    
+    file.close()
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_path', type=str, default='config/classify_mamba_config_oasis.yaml')
